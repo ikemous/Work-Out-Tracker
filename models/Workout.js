ba463629d85516b0af7589b9d1529505f5f1f69c
ike barranco <barranco.ike@outlook.com>
2020-07-06 20:00:33 -0600

    added schema to models page

---
+++ models/Workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        date: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Exercise Type Is Required"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Exercise Name Is Required"
+                },
+                duration: {
+                    type: Number,
+                    required: "Exercise Duration Needs To Be In Minutes"
+                },
+                weight: Number,
+                reps: Number,
+                sets: Number,
+                distance: Number
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+    
+);
+
+//type: {
+//     type: String,
+//     trim: true,
+//     required: "Exercise type is required"
+// }
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
