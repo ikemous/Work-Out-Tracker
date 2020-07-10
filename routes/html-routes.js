cccd2abb30a825278a4f0240a7d1ab4fc4960485
ike barranco <barranco.ike@outlook.com>
2020-07-10 07:23:29 -0600

    removed / route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,8 +9,4 @@ router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
 module.exports = router;
